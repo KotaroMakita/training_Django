12f383d679e07222e80668eb97dedd45059e29a7
kotaro_makita <makita2038@gmail.com>
2021-01-19 14:50:39 +0900

    解決！！タイプミス！！

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -40,7 +40,7 @@ def vote(request, question_id):
     except (KeyError, Choice.DoesNotExist):
         return render(request,'polls/detail.html',{'question':question,'error_message':"You didn't select a choice. ",})
     else:
-        selected_choice.vote += 1
+        selected_choice.votes += 1
         selected_choice.save()
 
     return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
