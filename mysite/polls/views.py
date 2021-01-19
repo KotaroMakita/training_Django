5ca401c961b8b4471dee7ed24671d926f8de323c
kotaro_makita <makita2038@gmail.com>
2021-01-19 14:48:35 +0900

    voteすると、AttributeError at /polls/1/vote/ 'Choice' object has no attribute 'vote'が出る。

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -37,7 +37,7 @@ def vote(request, question_id):
     question = get_object_or_404(Question,pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExitst):
+    except (KeyError, Choice.DoesNotExist):
         return render(request,'polls/detail.html',{'question':question,'error_message':"You didn't select a choice. ",})
     else:
         selected_choice.vote += 1
