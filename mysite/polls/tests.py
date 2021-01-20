25d00d248bcf42280d6c70b3e390fb39c302388a
kotaro_makita <makita2038@gmail.com>
2021-01-20 14:58:14 +0900

    testを書いた。（バグをあぶり出すためのテストを作成する）

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """was_published_recently() returns False for quesitons whose pub_date is in teh future."""
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_quesiton = Question(pub_date=time)
+        self.assertIs(future_quesiton.was_published_recently(),False)
